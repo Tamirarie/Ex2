6a919f7f289d59acb81bf47b1fa62b2881d15bac
Tamir arie <comp-2@comp-2-PC>
2016-04-21 20:17:48 +0300

    Fixing and adding methods in NmeaToDB

--- NmeaToDB.py
+++ NmeaToDB.py
@@ -1,7 +1,22 @@
+import os.path
 import sqlite3
 import csv
 from datetime import datetime
 import math
+
+def Createtime(value):
+    hour = value[:2]
+    minute = value[2:4]
+    second = value[4:6]
+    timeval = hour + ":" + minute + ":" + second
+    return timeval
+
+def Createdate(value):
+    day = value[:2]
+    month = value[2:4]
+    year = value[4:6]
+    dateval = "20"+year+"-"+month+"-"+day+"T"
+    return dateval
 
 def nmeaRMC(INPUT):
     conn = sqlite3.connect('example.db')
@@ -9,7 +24,8 @@ def nmeaRMC(INPUT):
     # Create table
 
     ## at the current we only create a table of nmeaRMC type
-    c.execute('''CREATE TABLE IF NOT EXISTS nmeaRMC
+    c.execute('drop table if exists nmea' + str(INPUT))
+    c.execute('''CREATE TABLE IF NOT EXISTS nmeaRMC'''+ str(INPUT)+'''
              (time text,warning text,latitude text,north text,
              longitude text,east text,
              speed text, date text )''')
@@ -71,20 +87,25 @@ def nmeaRMC(INPUT):
             # Save (commit) the changes
         conn.commit()
 
-         # We can also close the connection if we are done with it.
-         # Just be sure any changes have been committed or they will be lost.
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
         conn.close()
 
-def nmeaGGA(INPUT):
+def nmeaGGA(INPUT,TableName):
     conn = sqlite3.connect('example.db')
     c = conn.cursor()
+    l = str(TableName)
+    list = l.split(sep='.')
+    print(list)
     # Create table
-    c.execute('''CREATE TABLE IF NOT EXISTS nmeaGGA
+    c.execute('drop table if exists ' + str(list[0]) )
+    c.execute('''CREATE TABLE '''+ str(list[0]) + '''
              (time text,latitude text,north text,longitude text,east text,
              fix_quality text,numOfSat text,hdop text,altitude text,
             meters text, heightOfGeoid text, timeLU text, checkSum text )''')
     
     with open(INPUT, 'r') as input_file:
+        print(input_file)
         reader = csv.reader(input_file)
         # iterate over all the rows in the nmea file
         for row in reader:
@@ -94,7 +115,7 @@ def nmeaGGA(INPUT):
                 continue
 
             else:
-                 # for each row, fetch the values from the row's columns
+                # for each row, fetch the values from the row's columns
                 # columns that are not used contain technical GPS stuff that you are probably not interested in
                 time = row[1]
                 lat = row[2]
@@ -112,26 +133,29 @@ def nmeaGGA(INPUT):
 
 
                 # merge the time and date columns into one Python datetime object (usually more convenient than having both separately)
-                date_and_time = datetime.strptime(time, '%H%M%S.%f')
-
+                ##date_and_time = datetime.strptime(time, '%H%M%S.%f')
+                time = Createtime(time)
                 # convert the Python datetime into your preferred string format, see http://www.tutorialspoint.com/python/time_strftime.htm for futher possibilities
-                date_and_time = date_and_time.strftime('%H:%M:%S.%f')[:-3] # [:-3] cuts off the last three characters (trailing zeros from the fractional seconds)
+                timeLU = Createdate(timeLU)
 
                 # lat and lon values in the $GPRMC nmea sentences come in an rather uncommon format. for convenience, convert them into the commonly used decimal degree format which most applications can read.
                 # the "high level" formula for conversion is: DDMM.MMMMM => DD + (YY.ZZZZ / 60), multiplicated with (-1) if direction is either South or West
                 # the following reflects this formula in mathematical terms.
                 # lat and lon have to be converted from string to float in order to do calculations with them.
                 # you probably want the values rounded to 6 digits after the point for better readability.
-                lat = round(math.floor(float(lat) / 100) + (float(lat) % 100) / 60, 6)
-                if lat_direction == 'S':
-                    lat = lat * -1
-
-                lon = round(math.floor(float(lon) / 100) + (float(lon) % 100) / 60, 6)
-                if lon_direction == 'W':
-                    lon = lon * -1
+                
+                if isinstance( lat, ( int, float )):
+                    lat = round(math.floor(float(lat) / 100) + (float(lat) % 100) / 60, 6)
+                    if lat_direction == 'S':
+                        lat = lat * -1
+                    
+                if isinstance( lon, ( int, float )):
+                    lon = round(math.floor(float(lon) / 100) + (float(lon) % 100) / 60, 6)
+                    if lon_direction == 'W':
+                       lon = lon * -1
                 
                 # Insert a row of data
-                c.execute("INSERT INTO nmeaGGA VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                c.execute("INSERT INTO " + str(list[0]) + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
                         (time,lat,lat_direction,lon,lon_direction,fix_quality,
                          numOfSat,hdop,altitude,meters,heightofGeoid,timeLU,
                          checkSum)
@@ -140,12 +164,36 @@ def nmeaGGA(INPUT):
         # Save (commit) the changes
         conn.commit()
 
-         # We can also close the connection if we are done with it.
-         # Just be sure any changes have been committed or they will be lost.
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
         conn.close()
 
+def read_dir(dir_name):
+    global total
+    i = 'a'
+    if os.path.isdir(dir_name):
+        l = os.listdir(dir_name)
+        for k in range(len(l)):
+            ##sif ".nmea" in l[i]:
+                nmeaGGA(dir_name + "\\"+l[k],l[k])
+                i = i+'1'
+                
 
-INPUT = 'NMEAfiles\stockholm_walk.nmea'
-nmeaGGA(INPUT)
+
+def dropAll():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    tables = list(c.execute("select name from sqlite_master where type is 'table'"))
+
+    c.executescript(';'.join(["drop table if exists %s" % i for i in tables]))    
+    
+    
+INPUT = 'NMEAfiles'
+dropAll()
+##nmeaGGA(INPUT+"\FlightLog.nmea",'FlightLog')
+
+read_dir(INPUT)
+
+##nmeaGGA(INPUT)
 ##nmeaRMC(INPUT)
 
